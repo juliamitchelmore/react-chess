9a6a4937d47859bb388015f73c2423856fd75534
Julia Mitchelmore <julia@juliamitchelmore.com>
2016-01-23 16:43:01 +0000

    move variables out of state

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,88 +10,75 @@ import Knight from './Knight';
 const App = React.createClass({
     getInitialState() {
         return {
-            dragging: '',
-            mouseX: '',
-            mouseY: '',
-            knightX: '',
-            knightY: '',
             knightLeft: '',
-            knightTop: '',
-            knightLastX: '',
-            knightLastY: ''
+            knightTop: ''
         }
     },
 
     componentDidMount() {
+        this.dragging = false;
+        this.knightLastX = 55;
+        this.knightLastY = 55;
+
         return (
             this.setState({
-                dragging: false,
-                mouseX: 0,
-                mouseY: 0,
-                knightX: 0,
-                knightY: 0,
-                knightLeft: 0,
-                knightTop: 0,
-                knightLastX: 0,
-                knightLastY: 0
+                knightLeft: this.knightLastX,
+                knightTop: this.knightLastY
             })
         )
     },
 
     dragInit() {
         //set dragging
-        var obj = ReactDOM.findDOMNode(this.refs.knight);
-        this.setState({
-            dragging: true,
-            knightX: this.state.mouseX - obj.offsetLeft,
-            knightY: this.state.mouseY - obj.offsetTop
-        })
+        var knight = ReactDOM.findDOMNode(this.refs.knight);
+        this.dragging = true;
+        this.knightX = this.mouseX - knight.offsetLeft;
+        this.knightY = this.mouseY - knight.offsetTop;
     },
 
     handleMouseMove(e) {
         //capture mouse coordinates
-        this.setState({
-            mouseX: e.clientX,
-            mouseY: e.clientY
-        })
+        this.mouseX = e.clientX;
+        this.mouseY = e.clientY;
 
         //detect whether mouse within board bounds
-        var board = ReactDOM.findDOMNode(this.refs.board),
-            boardLeft = board.offsetLeft + 5,
-            boardTop = board.offsetTop + 5;
-        var insideX = this.state.mouseX >= boardLeft && this.state.mouseX <= boardLeft + board.clientWidth,
-            insideY = this.state.mouseY >= boardTop && this.state.mouseY <=  boardTop + board.clientHeight;
+        this.board = ReactDOM.findDOMNode(this.refs.board);
+        var boardLeft = this.board.offsetLeft + 5,
+            boardTop = this.board.offsetTop + 5;
+        this.insideX = this.mouseX >= boardLeft && this.mouseX <= boardLeft + this.board.clientWidth;
+        this.insideY = this.mouseY >= boardTop && this.mouseY <=  boardTop + this.board.clientHeight;
 
         //set new coordinates for knight
-        if (this.state.dragging && insideX && insideY) {
+        if (this.dragging && this.insideX && this.insideY) {
             this.setState({
-                knightLeft: (this.state.mouseX - this.state.knightX) + 'px',
-                knightTop: (this.state.mouseY - this.state.knightY) + 'px'
+                knightLeft: this.mouseX - this.knightX,
+                knightTop: this.mouseY - this.knightY
             })
         }
     },
 
     destroy() {
-        var board = ReactDOM.findDOMNode(this.refs.board),
-            boardLeft = board.offsetLeft + 5,
-            boardTop = board.offsetTop + 5;
+        this.dragging = false;
 
-        var insideX = this.state.mouseX >= boardLeft && this.state.mouseX <= boardLeft + board.clientWidth,
-            insideY = this.state.mouseY >= boardTop && this.state.mouseY <=  boardTop + board.clientHeight;
+        var boardLeft = this.board.offsetLeft + 5,
+            boardTop = this.board.offsetTop + 5;
 
-        //detect which square mouse is currently over. if mouse outside board, snap back to last position
-        var mouseXCoord = Math.floor((this.state.mouseX - board.offsetLeft + 5) / (board.clientWidth / 8)),
-            mouseYCoord = Math.floor((this.state.mouseY - board.offsetTop + 5) / (board.clientHeight / 8)),
-            snapX = insideX && insideY ? mouseXCoord * (board.clientWidth / 8) + boardLeft : this.state.knightLastX,
-            snapY = insideY && insideX ? mouseYCoord * (board.clientHeight / 8) + boardTop : this.state.knightLastY;
+        //detect which square mouse is currently over from 0 to 7
+        var mouseXCoord = Math.floor((this.mouseX - boardLeft) / (this.board.clientWidth / 8)),
+            mouseYCoord = Math.floor((this.mouseY - boardTop) / (this.board.clientHeight / 8));
+
+        //if mouse outside board, snap back to last position. else, snap the knight to the middle of the square
+        var snapX = this.insideX && this.insideY ? mouseXCoord * (this.board.clientWidth / 8) + boardLeft : this.knightLastX,
+            snapY = this.insideY && this.insideX ? mouseYCoord * (this.board.clientHeight / 8) + boardTop : this.knightLastY;
+
+        //set last snapped
+        this.knightLastX = snapX;
+        this.knightLastY = snapY;
 
         //cancel drag & snap knight to centre of new square
         this.setState({
-            dragging: false,
             knightLeft: snapX + 'px',
-            knightTop: snapY + 'px',
-            knightLastX: snapX,
-            knightLastY: snapY
+            knightTop: snapY + 'px'
         })
     },
 
